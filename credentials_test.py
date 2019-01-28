cb1d2d0e9f15b7a588fc00e51e08b8ee313e35ad
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-28 16:35:35 +0300

    test_save_credential test case to test if the condential object is saves into

--- credentials_test.py
+++ credentials_test.py
@@ -19,14 +19,19 @@ class TestCredential(unittest.TestCase):
         tearDown method that does clean up after each test case has run
         '''
         Credential.credential_list = []
-    
+
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
-        self.assertEqual(self.new_credential.view_password,"12345")
-        self.assertEqual(self.new_credential.account,"instagram")
-        self.assertEqual(self.new_credential.login,"milkshake")
-        self.assertEqual(self.new_credential.password,"12345")
-        
-          
+        self.assertEqual(self.new_credential.view_password, "12345")
+        self.assertEqual(self.new_credential.account, "instagram")
+        self.assertEqual(self.new_credential.login, "milkshake")
+        self.assertEqual(self.new_credential.password, "12345")
+
+    def test_save_credential(self):
+        """
+        test_save_credential test case to test if the credential object is saves into
+        """
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list), 1)
