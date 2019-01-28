7f3b19df8491f95c3a8fa1194e04d464b39f6d89
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-28 16:46:04 +0300

    test display all credentials to return a list of all credentials

--- credentials_test.py
+++ credentials_test.py
@@ -45,6 +45,11 @@ class TestCredential(unittest.TestCase):
         test_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),2)
 
-    
+    def test_display_all_credentials(self):
+        '''
+        test_display_all_credentials to returns a list of all credentials saved
+        '''
+        self.assertEqual(Credential.display_credentials(),Credential.credential_list)
+
     if __name__ == '__main__':
         unittest.main()
