f094c2475ab2fa4702832ae59d9a0f05d38b261c
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-28 14:27:41 +0300

    add main function

--- run.py
+++ run.py
@@ -1,16 +1,34 @@
 from user import User
 
 
-def create_user(username, password):
+def create_user_account(username, password):
     '''
-    Function to create a new user
+    Function to create a new user account
     '''
     new_user = User(username, password)
     return new_user
 
 
-def save_user(user):
+def save_user_account(user):
     '''
-    Function to save user
+    Function to save user account
     '''
     user.save_user()
+
+
+def check_existing_users(characters):
+    '''
+    Function that checks if a User exists with those characters and return Boolean
+    '''
+    return User.user_exist(characters)
+
+
+def main():
+    print("Hello Welcome to your Pass-locker. What is your name?")
+    user_name = input()
+
+    print(f"Hello {user_name}.")
+    print('\n')
+    while true:
+            print("\nUse these short codes below:")
+            print
