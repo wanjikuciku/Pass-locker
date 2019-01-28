9ea075190da0e8ef42d9075ea7df4754ac5a8ed7
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-28 12:17:05 +0300

    add code to uder.py to pass the test

--- user.py
+++ user.py
@@ -13,3 +13,18 @@ class User:
         save_user method saves user objects into user_list
         '''
         User.user_list.append(self)
+
+    @classmethod
+    def user_exists(cls, characters):
+        '''
+        Method that checks if a user exists from the user list.
+        Args:
+            character:username to search if it exists
+        Returns:
+            Boolean:true or false depending on the condition
+        '''
+        for user in cls.user_list:
+            if user.password == characters:
+                return True
+
+        return False
