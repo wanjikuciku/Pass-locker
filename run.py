6e5424df43a365c1c5e56694fe4f48c15beb390a
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-28 12:42:55 +0300

    add save contacts function to run.py

--- run.py
+++ run.py
@@ -7,3 +7,10 @@ def create_user(username, password):
     '''
     new_user = User(username, password)
     return new_user
+
+
+def save_user(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
